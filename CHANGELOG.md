02c1804c18fb357f8c452b1a8c99ed18ab5cd198
Kiko Beats <josefrancisco.verdu@gmail.com>
2015-12-13 21:14:12 +0100

    1.0.1 releases

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,16 @@
+<a name="1.0.1"></a>
+## 1.0.1 (2015-12-13)
+
+
+* Update keywords ([0d20bbc](https://github.com/kikobeats/async.whilst/commit/0d20bbc))
+
+
+
 <a name="1.0.0"></a>
 # 1.0.0 (2015-12-13)
 
 
+* 1.0.0 releases ([31bb336](https://github.com/kikobeats/async.whilst/commit/31bb336))
 * Complete description ([949bb54](https://github.com/kikobeats/async.whilst/commit/949bb54))
 * Initial commit ([c68af28](https://github.com/kikobeats/async.whilst/commit/c68af28))
 
--- dist/async.whilst.js
+++ dist/async.whilst.js
@@ -1,6 +1,6 @@
 /**
  * async.whilst - Call a function and stop under condition or error occurs.
- * @version v1.0.0
+ * @version v1.0.1
  * @link    https://github.com/Kikobeats/async.whilst
  * @license MIT
  */require=function r(t,n,e){function o(a,i){if(!n[a]){if(!t[a]){var c="function"==typeof require&&require;if(!i&&c)return c(a,!0);if(u)return u(a,!0);var f=new Error("Cannot find module '"+a+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[a]={exports:{}};t[a][0].call(l.exports,function(r){var n=t[a][1][r];return o(n?n:r)},l,l.exports,r,t,n,e)}return n[a].exports}for(var u="function"==typeof require&&require,a=0;a<e.length;a++)o(e[a]);return o}({1:[function(r,t,n){function e(r,t){if("function"!=typeof r)throw new TypeError(o);return t=u(void 0===t?r.length-1:+t||0,0),function(){for(var n=arguments,e=-1,o=u(n.length-t,0),a=Array(o);++e<o;)a[e]=n[t+e];switch(t){case 0:return r.call(this,a);case 1:return r.call(this,n[0],a);case 2:return r.call(this,n[0],n[1],a)}var i=Array(t+1);for(e=-1;++e<t;)i[e]=n[e];return i[t]=a,r.apply(this,i)}}var o="Expected a function",u=Math.max;t.exports=e},{}],"async.whilst":[function(r,t,n){"use strict";var e=function(){},o=r("lodash.restparam");t.exports=function(r,t,n){n=n||e,r()||n(null);var u=o(function(e,o){return e?n(e):void(r.apply(this,o)?t(u):n.apply(null,[null].concat(o)))});t(u)}},{"lodash.restparam":1}]},{},[]);
